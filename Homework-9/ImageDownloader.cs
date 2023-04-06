21920e3785dd0066f037019b6908b21d696d9af6
Дмитрий Хаткевич <dmitry.hatkevich@yandex.ru>
2023-04-06 23:29:39 +0300

    Добавьте файлы проекта.

---
+++ Homework-9/ImageDownloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_9
+{
+    internal class ImageDownloader
+    {
+        public Task rezDownload;
+        public void Download(string url)
+        {
+            // Откуда будем качать
+            string remoteUri = url;
+            // Как назовем файл на диске
+            string fileName = "bigimage.jpg";
+            // Качаем картинку в текущую директорию
+            var myWebClient = new WebClient();
+            ImageStarted?.Invoke();
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n", fileName, remoteUri);
+            
+            rezDownload = myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            
+            ImageCompleted?.Invoke();
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+            
+        }
+
+        public event Action ImageStarted;
+        public event Action ImageCompleted;
+    }
+}
---
+++ Homework-9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Обьявляем экземпляр класса
+            ImageDownloader downloader = new ImageDownloader();
+
+            //Подписываемся на события
+            downloader.ImageStarted += Downloader_ImageStarted;
+            downloader.ImageCompleted += Downloader_ImageCompleted;
+            
+            //Вызываем метод для загрузки картинки по урлу
+            downloader.Download("https://prixoxo.ru/uploads/posts/2021-08/fioletovyj-cvet-kartinki-i-krasivye-foto-3.jpg");
+
+            Console.WriteLine("Нажмите клавишу A для выхода или любую другую клавишу для проверки статуса скачивания");
+            var key = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            //Обработка нажатой клавиши
+            KeyDown(key, downloader);
+        }
+
+        
+
+        //Обработчики событий
+        private static void Downloader_ImageCompleted()
+        {
+            Console.WriteLine("Скачивание файла закончилось");
+        }
+
+        private static void Downloader_ImageStarted()
+        {
+            Console.WriteLine("Скачивание файла началось");
+        }
+
+        //Обработка нажатия клавиш
+        public static void KeyDown(char key, ImageDownloader downloader)
+        {
+            if (key == 'A' || key == 'a')
+            {
+                Environment.Exit(0);
+            }
+
+            if (downloader.rezDownload.IsCompleted)
+            {
+                Console.WriteLine("Cостояние загрузки картинки - загружена");
+                return;
+            }
+
+            Console.WriteLine("Cостояние загрузки картинки - не загружена");
+        }
+    }
+}
